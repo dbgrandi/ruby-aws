3583be885f8ffbefdafa6ac3d319b63f7794e9c2
valthon <valthon@54d3e444-7fb1-497b-8161-fb1b7e735945>
2007-07-18 22:48:12 +0000

    adding a new sample app: BlankSlate
    git-svn-id: http://ruby-aws.rubyforge.org/svn/trunk@18 54d3e444-7fb1-497b-8161-fb1b7e735945

---
+++ samples/mturk/blank_slate/BlankSlate.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+
+# Copyright:: Copyright (c) 2007 Amazon Technologies, Inc.
+# License::   Apache License, Version 2.0
+
+# The BlankSlate sample application disposes all of your HITs on sanbox
+
+require 'ruby-aws'
+@mturk = Amazon::WebServices::MechanicalTurkRequester.new :Host => :Sandbox
+
+def forceExpire(id)
+  print "Ensuring HIT #{id} is expired: "
+  begin
+    @mturk.forceExpireHIT( :HITId => id )
+  rescue => e
+    raise e unless e.message == 'AWS.MechanicalTurk.InvalidHITState'
+  end
+  puts "OK"
+end
+
+def dispose(id)
+  print "Disposing HIT #{id}: "
+  @mturk.disposeHIT( :HITId => id )
+  puts "OK"
+end
+
+def purge
+  hit_ids = @mturk.searchHITsAll.collect {|hit| hit[:HITId] }
+  puts "Found #{hit_ids.size} HITs"
+
+  return false if hit_ids.size == 0
+
+  hit_ids.each do |id|
+    begin
+      forceExpire id
+      dispose id
+    rescue => e
+      puts e.message
+    end
+  end
+  
+  return true
+end
+
+while purge
+  puts 'Ensuring there are no more hits...'
+end
+
+puts 'You now have a blank slate'
